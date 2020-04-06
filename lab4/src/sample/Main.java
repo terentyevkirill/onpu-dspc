30faaaeb2aba650fc5e99b4cc4e8ece859727416
terentyevkirill <kirillterentyev1999@gmail.com>
2020-04-06 18:22:07 +0200

    create layout file, setup window size

--- lab4/src/sample/Main.java
+++ lab4/src/sample/Main.java
@@ -12,7 +12,8 @@ public class Main extends Application {
     public void start(Stage primaryStage) throws Exception{
         Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
         primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
+        primaryStage.setScene(new Scene(root, 800, 450));
+        primaryStage.setResizable(false);
         primaryStage.show();
     }
 
